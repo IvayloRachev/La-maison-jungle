b96c0a7e4624bc158d89542a28b7ba74b42bff86
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-02-17 18:29:10 +0100

    Summary

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Cart from './Cart';
 
 const Banner = () => {
     return (
         <div>
             <h1>La maison jungle</h1>
+            <Cart />
         </div>
     );
 };
